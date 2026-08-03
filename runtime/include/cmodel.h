c8dbcc33fd009771fefcd9be1a9b5783422c4c07
DragonSerana <2458649263@qq.com>
2026-08-03 22:58:11 +0800

    add cmodel/simulator

---
+++ runtime/include/cmodel.h
@@ -0,0 +1,13 @@
+#ifndef CMODEL_H
+#define CMODEL_H
+
+#include "eclipse_isa.h"
+#include <vector>
+
+Class CModel {
+    std::vector<uint8_t> ddr_, sram_;
+private:
+    
+};
+
+#endif
---
+++ runtime/include/eclipse_isa.h
@@ -0,0 +1,49 @@
+#ifndef ECLIPSE_ISA_H
+#define ECLIPSE_ISA_H
+
+#define SRAM_ADDR 0x10000000
+#define DDR_ADDR 0x80000000
+
+#define SRAM_SIZE 0x80000
+#define DDR_SIZE 0x40000000
+
+typedef unsigned int uint32_t;
+typedef unsigned char uint8_t;
+
+struct dma_opcode_param { 
+    uint32_t sram_addr;
+    uint32_t ddr_addr; 
+    uint32_t rows; 
+    uint32_t cols;
+    uint32_t src_stride;
+    uint32_t dst_stride;
+};
+
+struct matmul_opcode_param { 
+    uint32_t dst_addr;
+    uint32_t rhs_addr;
+    uint32_t lhs_addr;
+    uint32_t M;
+    uint32_t K;
+    uint32_t N;
+    uint32_t accumulate;
+};
+
+struct elementwise_add_opcode_param { 
+    uint32_t dst_addr;
+    uint32_t rhs_addr;
+    uint32_t lhs_addr;
+    uint32_t n;
+};
+
+struct act_opcode_param {
+    uint32_t dst_addr;
+    uint32_t src_addr;
+    uint32_t n;
+    uint32_t kind;
+    union { 
+        uint32_t extra[4];
+    };
+};
+
+#endif
